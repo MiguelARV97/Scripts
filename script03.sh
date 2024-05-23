3a3a53b1d58ca2686dae99d2b4b6ced817f8f4e1
MiguelARV97 <19030086@uan.edu.mx>
2024-05-22 20:49:02 -0700

    Delete script03.sh

--- script03.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Autor: Miguel
-#Fecha: 07-03-24
-#Descripcion: Manejo de parametros dentro de la inocacion de un script
-
-echo "El nombre del scipt es: $0 "
-echo "Los argumentos son: $1 - $2 -  $3 "
