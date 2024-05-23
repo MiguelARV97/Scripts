0ab0cfa40b993fc7f5f42837199591b89e6a6cd2
MiguelARV97 <19030086@uan.edu.mx>
2024-05-22 20:49:30 -0700

    Add files via upload

---
+++ script02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Autor: Miguel
+#Fecha: 07-03-24
+#Descripcion: Uso de variables de entorno
+clear
+echo $HOME
+echo $USER
+echo $PWD
---
+++ script04.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Autor: Miguel
+#Fecha: 07-03-24
+#Descripcion:Ejemplo de una funcion
+
+function aqui {
+	echo "Hola mundo $1 "
+}
+aqui $1
+
